dafa43392aa056fe58552aef1bf4a7772d8fadae
AakashNiwane <35794294+AakashNiwane@users.noreply.github.com>
2022-08-25 21:37:00 +0530

    Adding Button and clickEvent calculation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import './App.css';
-import React from 'react';
-import Header from './components/Header';
+import "./App.css";
+import React from "react";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import Header from "./components/Header";
+import Buttons from "./components/Buttons";
+import reducer from "./reducers";
 
 function App() {
   return (
-      <div className ="calc-app">
-        <div className="calc-wrapper">
-          <Header />
-        </div>
+    <div className="calc-app">
+      <div className="calc-wrapper">
+        <Header />
+        <Buttons />
       </div>
-    );
+    </div>
+  );
 }
 
-const wrapped = (
-		<App />
-);
+const store = createStore(reducer);
 
+const wrapped = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
 export default wrapped;
